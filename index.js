e8cfcdd4fb5e8a050c8241f44f3ece2cf01044d0
fddhrfjgjc <sankpaladitya22@gmail.com>
2026-04-03 14:46:12 +0530

    Add root server entrypoint for Render

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./backend/app');
+const app = require('./backend/app');
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
